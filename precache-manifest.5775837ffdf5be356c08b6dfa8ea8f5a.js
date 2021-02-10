4604b1ff6616525e6c35f3e85a40fa47bffd508c
DychekNikita <62640390+DychekNikita@users.noreply.github.com>
2021-02-10 15:47:11 +0300

    Updates

---
+++ precache-manifest.5775837ffdf5be356c08b6dfa8ea8f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a22fcd47b39817a36888e6950b2d14b",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "f7c98f356a2932aba141",
+    "url": "/spacex/static/css/main.e43ee2c5.chunk.css"
+  },
+  {
+    "revision": "2453b001a309e6550510",
+    "url": "/spacex/static/js/2.17062263.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spacex/static/js/2.17062263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c98f356a2932aba141",
+    "url": "/spacex/static/js/main.7fb8ba48.chunk.js"
+  },
+  {
+    "revision": "38bd822f86f0fa518f7f",
+    "url": "/spacex/static/js/runtime-main.3f7de82f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spacex/static/media/logo.d5ab31ce.svg"
+  }
+]);
